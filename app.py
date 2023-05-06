61f047d0f35a393385d9d6ad61d7f6552c8ae0bd
MicroOptimization <jackiezhen538@gmail.com>
2023-05-06 15:48:34 -0400

    updated readme and also finished the core functionality, which is the ability to exclude directories, certain extensions, and file literals

--- app.py
+++ app.py
@@ -2,9 +2,55 @@ import sys
 import os
 import zipfile
 
-#"C:\Python38\python.exe" "C:\Users\Codia\Desktop\dev projects\ignore_zipper\app.py" "%V"
-print()
+def zipdir(path, archive, exclusions):
+    # Iterate over all files in the directory
+    for root, dirs, files in os.walk(path): #root is cur dir path
+        name = archive.filename
+        for file in files:
+            # Get the full (absolute) file path
+            file_path = os.path.join(root, file)
+            #print(file_path)
+            #print("\\old datasets\\" in file_path)
+            #chopping that absolute path into a relative path
+            
+            dir_check = file_path[len(path):]
+            file_path = dir_check[1:]
+            extension = "." + file.split(".")[-1]
+            
+            # Add the file to the ZIP archive after checking if it's on the exclusion list
+            if (file in exclusions or extension in exclusions):
+                continue;
+            
+            found_excluded_directory = False;
+            for dir in excludedDirs:
+                if dir in dir_check:
+                    found_excluded_directory = True;
+                    break
+    
+            if not found_excluded_directory:
+                archive.write(file_path)
+
+def getExclusions(exclusionSet):
+    filename = "exclude.txt"  # Replace with the name of your file
+    excludedDirs = set()
+    if not os.path.exists(filename):
+        print("No exclude.txt found");
+        return
+    
+    with open(filename, "r") as file:
+        for line in file:
+            # Strip the newline character and add the string to the set
+            str = line.strip()
+            if ("." not in str):
+                excludedDirs.add("\\" + str + "\\")
+            else:
+                exclusionSet.add(str)
+    
+    return exclusionSet, excludedDirs
+
+
 print("This is CDX's zip utility.");
+
 testVal = "C:/Users/Codia/Desktop/dev_projects/ignore_zipper/test_dir"
 if (len(sys.argv) > 1):
     curDir = sys.argv[1]
@@ -12,34 +58,21 @@ else:
     curDir = testVal
 print("Running in dir: " , curDir)
 
-def zipdir(path, archive):
-    # Iterate over all files in the directory
-    for root, dirs, files in os.walk(path): #root is cur dir path
-        name = archive.filename
-        for file in files:
-            # Get the full file path
-            file_path = os.path.join(root, file)
-            file_path = file_path[len(path) + 1:]
-            print(file_path)
-            # Add the file to the ZIP archive
-            if (file != name):
-                print("writing: " , file_path)
-                archive.write(file_path)
-
-
-# Specify the directory to zip and the output zip file name
-#dir_path = '/path/to/directory'
+exclusions = set()
 zip_file_name = 'output.zip'
+exclusions.add(zip_file_name)
+exclusions.add("exclude.txt")
 
-# Create a new ZIP archive
+exclusions, excludedDirs = getExclusions(exclusions);
+
+print("Excluded Files and Extensions: " , exclusions)
+print("Excluded Directories: " , excludedDirs)
+
+# Create a new ZIP archive file
 zipf = zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED)
-
-base_name = curDir.split("\\")[-1]
-print("base name: " , base_name)
-# Zip all files in the specified directory
-zipdir(curDir, zipf)
+zipdir(curDir, zipf, exclusions) 
 
 # Close the ZIP archive
 zipf.close()
 
-print("zipping complete")
+print("Zipping Complete")
--- readme.md
+++ readme.md
@@ -2,3 +2,21 @@
 
 Puts everything in the current directory in a zip file except for files 
 whose names are in a txt file called "exclude.txt"
+
+# Sample "exclude.txt"
+"""
+env
+.gitignore
+ex1.py
+ex2.txt
+ex3
+ex5.txt
+.zip
+"""
+# Current features:
+- Can be run as a CLI with the cdxzip command
+- Can also be put on regedit to run from a context menu
+- Excludable files:
+  - File Literals
+  - Directories
+  - Extensions
